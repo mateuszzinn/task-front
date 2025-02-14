ee9e03bf5471c3c9c20fbba6c1266a23c2d06289
mateuszzinn <mateuslima2002.ml@gmail.com>
2025-02-13 21:50:29 -0300

    task 03 concluida

--- src/components/cardsApi/cardApi.tsx
+++ src/components/cardsApi/cardApi.tsx
@@ -3,7 +3,17 @@ import useCharacter from "@/app/hooks/useCharacter";
 export default function CharacterCard({ id }: { id: number }) {
   const { character, loading, error } = useCharacter(id);
 
-  if (loading) return <p className="text-white">Carregando...</p>;
+  if (loading)
+    return (
+      <div className="p-4 border rounded-lg shadow-md bg-gray-900 text-white w-72 text-center">
+        <div className="min-w-[254px] min-h-[288px] bg-gray-700 animate-pulse"></div>
+        <div className="flex justify-center flex-col items-center animate-pulse gap-1">
+          <h2 className="text-lg font-bold mt-2 max-w-[150px] min-w-[150px] min-h-[28px] bg-gray-700"></h2>
+          <h2 className="max-w-[150px] min-w-[150px] min-h-[28px] bg-gray-700"></h2>
+          <h2 className="max-w-[150px] min-w-[150px] min-h-[28px] bg-gray-700"></h2>
+        </div>
+      </div>
+    );
   if (error) return <p className="text-red-500">Erro: {error}</p>;
 
   return (
